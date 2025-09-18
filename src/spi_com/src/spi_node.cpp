fca5cd50c01eaf2c6da59f977ab7085e0ba6bf92
Erica Jiang <ericaijiang@gmail.com>
2025-09-18 18:06:32 -0400

    adc parsing

--- src/spi_com/src/spi_node.cpp
+++ src/spi_com/src/spi_node.cpp
@@ -78,15 +78,15 @@ private:
     int pi_{-1};
     rclcpp::TimerBase::SharedPtr timer_;
 
-    void readData(int chipSelect)
+    std::vector<uint8_t> readData(int chipSelect)
     {
 
         gpio_write(pi_, GPIO_CS1, chipSelect == 1 ? 0 : 1);
         gpio_write(pi_, GPIO_CS2, chipSelect == 2 ? 0 : 1);
         gpio_write(pi_, GPIO_CS3, chipSelect == 3 ? 0 : 1);
 
-        std::vector<uint8_t> tx(10, 0x00);  // Dummy data to send
-        std::vector<uint8_t> rx(10, 0x00);  // Buffer to store received data
+        std::vector<uint8_t> tx(10, 0x00);  
+        std::vector<uint8_t> rx(10, 0x00);  
 
         // SPI transfer structure   
         struct spi_ioc_transfer transfer = {};
@@ -96,29 +96,42 @@ private:
         transfer.speed_hz = SPI_SPEED;
         transfer.bits_per_word = 8;
 
-        if (ioctl(spi_fd_, SPI_IOC_MESSAGE(1), &transfer) < 0)
-        {
+        if (ioctl(spi_fd_, SPI_IOC_MESSAGE(1), &transfer) < 0) {
             RCLCPP_ERROR(this->get_logger(), "SPI transfer failed.");
-            return;
         }
-
-        RCLCPP_INFO(this->get_logger(), "Received SPI data: ");
-        for (int i = 0; i < 10; ++i)
-        {
-            RCLCPP_INFO(this->get_logger(), "0x%02X", rx[i]);
-        }
-
         
         gpio_write(pi_, GPIO_CS1, 1);
         gpio_write(pi_, GPIO_CS2, 1);
         gpio_write(pi_, GPIO_CS3, 1);
+
+        return rx;
+
+        // RCLCPP_INFO(this->get_logger(), "Received SPI data: ");
+        // for (int i = 0; i < 10; ++i)
+        // {
+        //     RCLCPP_INFO(this->get_logger(), "0x%02X", rx[i]);
+        // }
     }
 
     // Timer callback to periodically read from the SPI device
     void timer_callback() {
-        // uint8_t raw_value = read_pico();
-        // RCLCPP_INFO(this->get_logger(), "Raw Value: %d", raw_value);
-        readData(1); // Read from device with CS1
+        std::vector<uint8_t> rx = readData(1); 
+
+        uint32_t timestamp = 0;
+        uint16_t adc1 = 0;
+        uint16_t adc2 = 0;
+        uint16_t adc3 = 0;
+
+        timestamp = (rx[3]) | (rx[2] << 8) | (rx[1] << 16) | (rx[0] << 24);
+        adc1 = (rx[5]) | (rx[4] << 8);
+        adc2 = (rx[7]) | (rx[6] << 8);
+        adc3 = (rx[9]) | (rx[8] << 8);
+
+        RCLCPP_INFO(this->get_logger(), "Received SPI data:");
+        RCLCPP_INFO(this->get_logger(), "Timestamp: %u", timestamp);
+        RCLCPP_INFO(this->get_logger(), "ADC1: %u", adc1);  
+        RCLCPP_INFO(this->get_logger(), "ADC2: %u", adc2);  
+        RCLCPP_INFO(this->get_logger(), "ADC3: %u", adc3);  
     }
 };
 
