ac4c52c6b2da043a37d740a2f90e52506f1a5fa3
gerald <jji.bigg@gmail.com>
2025-09-20 15:33:10 -0400

    changes

--- pico/spi_sender/spi_sender.c
+++ pico/spi_sender/spi_sender.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/spi.h"
-#include "hardware/adc.h"
 #include "hardware/dma.h"
 
 #define LED 25
@@ -26,9 +25,10 @@ void restore_gpio_oe_control(uint pin) {
     io_bank0_hw->io[pin].ctrl = (io_bank0_hw->io[pin].ctrl & ~IO_BANK0_GPIO0_CTRL_OEOVER_BITS) | (IO_BANK0_GPIO0_CTRL_OEOVER_VALUE_NORMAL << IO_BANK0_GPIO0_CTRL_OEOVER_LSB);
 }
 
-uint16_t read_adc(uint input) {
-    adc_select_input(input);
-    return adc_read();  
+uint16_t read_spi_data() {
+    uint8_t rx_buffer[2];
+    spi_read_blocking(SPI_PORT, 0x00, rx_buffer, 2); // Read 2 bytes
+    return (rx_buffer[0] << 8) | rx_buffer[1]; // Combine into 16-bit value
 }
 
 void configure_dma() {
@@ -55,23 +55,23 @@ void irq_handler(uint gpio, uint32_t events) {
         gpio_set_function(PIN_TX, GPIO_FUNC_SPI);
         
         uint32_t now_us = (uint32_t)time_us_64();
-        uint16_t adc0 = read_adc(0);  
-        uint16_t adc1 = read_adc(1);  
-        uint16_t adc2 = read_adc(2);  
+        uint16_t spi_data0 = read_spi_data();  
+        uint16_t spi_data1 = read_spi_data();  
+        uint16_t spi_data2 = read_spi_data();  
 
         tx_data[0] = (now_us >> 24) & 0xFF;
         tx_data[1] = (now_us >> 16) & 0xFF;
         tx_data[2] = (now_us >> 8) & 0xFF;
         tx_data[3] = (now_us >> 0) & 0xFF;
 
-        tx_data[4] = (adc0 >> 8) & 0xFF;
-        tx_data[5] = (adc0 >> 0) & 0xFF;
+        tx_data[4] = (spi_data0 >> 8) & 0xFF;
+        tx_data[5] = (spi_data0 >> 0) & 0xFF;
 
-        tx_data[6] = (adc1 >> 8) & 0xFF;
-        tx_data[7] = (adc1 >> 0) & 0xFF;
+        tx_data[6] = (spi_data1 >> 8) & 0xFF;
+        tx_data[7] = (spi_data1 >> 0) & 0xFF;
 
-        tx_data[8] = (adc2 >> 8) & 0xFF;
-        tx_data[9] = (adc2 >> 0) & 0xFF;
+        tx_data[8] = (spi_data2 >> 8) & 0xFF;
+        tx_data[9] = (spi_data2 >> 0) & 0xFF;
 
         tx_data[10] = 0x00; // End byte
 
@@ -100,11 +100,6 @@ void initialize() {
     gpio_init(TRIGGER);
     gpio_set_dir(TRIGGER, GPIO_IN);
     gpio_set_irq_enabled_with_callback(TRIGGER, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &irq_handler);
-
-    adc_init();
-    adc_gpio_init(26);  // GPIO 26
-    adc_gpio_init(27);  // GPIO 27
-    adc_gpio_init(28);  // GPIO 28
 
     spi_init(SPI_PORT, 1000 * 1000);  
     spi_set_slave(SPI_PORT, true);   
